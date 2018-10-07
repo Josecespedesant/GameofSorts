b16bb6958d30c68706c4961bd7022e507e6bb676
dnsing <33098069+dnsing@users.noreply.github.com>
2018-10-07 01:35:06 -0600

    Prototipo interfaz

---
+++ GameofSorts/src/Interface/Board.java
@@ -0,0 +1,66 @@
+package Interface;
+
+import java.awt.*;
+import java.awt.Image;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+import entities.Player;
+
+public class Board extends JPanel implements ActionListener{
+	Player p;
+	Image img;
+	Timer time;
+	int nx, nx2;
+
+	public Board() {
+		p = new Player();
+		addKeyListener(new AL());
+		setFocusable(true);
+		ImageIcon i = new ImageIcon("GoSBG.jpeg");	
+		img = i.getImage();
+		time = new Timer(5, this);
+		time.start();
+		nx = 0;
+		nx2 = 1266;
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		p.move();
+		repaint();
+	}
+	
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		
+		nx--;
+		nx2--;
+		g2d.drawImage(img, nx, 0, null);
+		if (nx <= 0) {
+			g2d.drawImage(img, nx2+100, 0, null);
+			System.out.println(nx);
+		}
+		if (nx == -1370) {
+			nx =0;
+			nx2 = 1266;
+		}
+		
+		g2d.drawImage(p.getImage(), p.getX(), p.getY(), null);
+	}
+	
+	private class AL extends KeyAdapter{
+		
+		public void keyReleased(KeyEvent e) {
+			p.KeyReleased(e);
+		}
+		
+		public void keyPressed(KeyEvent e) {
+			p.KeyPressed(e);
+		}
+		
+	}
+}
--- GameofSorts/src/entities/Player.java
+++ GameofSorts/src/entities/Player.java
@@ -1,5 +1,91 @@
 package entities;
 
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+
+import javax.swing.ImageIcon;
+
 public class Player {
+	public int x, dx, y, dy, nx, nx2;
+	Image peter;
+	ImageIcon i = new ImageIcon("Peter_Griffin.png");
+	ImageIcon l = new ImageIcon("Peter_Griffin_Left.png");
+	
+	public Player() {
+
+		peter = i.getImage();
+		x = 10;
+		y = 350;
+		nx = 0;
+		nx2 = 1266;
+	}
+	
+	public void move() {
+		//if(dx!=1) {
+		x = x + dx;
+		y = y + dy;
+		//nx2 = nx2 + dx;
+		//nx = nx + dx;
+		
+	//	}
+	}
+	
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+	
+	public int getNx2() {
+		return nx2;
+	}
+	public void setNx2(int nx2) {
+		this.nx2 = nx2;
+	}
+	
+	public int getNx() {
+		return nx; 
+	}
+	public void setNx(int nx) {
+		nx = this.nx;
+	}
+	
+	public Image getImage() {
+		return peter;
+	}
+	
+	public void KeyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+		if(key == KeyEvent.VK_LEFT) { 
+			dx = -2;
+			//peter = l.getImage();
+		}
+		if(key == KeyEvent.VK_RIGHT) {
+			dx = +1;
+			peter = i.getImage();
+		}
+		if(key == KeyEvent.VK_UP)
+			dy = -1;
+		if(key == KeyEvent.VK_DOWN)
+			dy = 1;
+	}
+	
+	public void KeyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
+		if(key == KeyEvent.VK_LEFT) 
+			dx = -1;
+		if(key == KeyEvent.VK_RIGHT)
+			dx = -1;
+		if(key == KeyEvent.VK_DOWN)
+			dy = 0;
+			dx = -1;
+		if(key == KeyEvent.VK_UP)
+			dy = 0;
+			dx = -1;
+	}
+
 	
 }
