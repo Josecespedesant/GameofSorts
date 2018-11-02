6a41fc4d39ed267edd4ff64da884d3eac8e0bbe1
José Antonio Céspedes Downing <tonio782009@hotmail.com>
2018-11-01 22:20:52 -0600

    Cambio en pantalla de inicio

--- src/Interface/BoardInicio.java
+++ src/Interface/BoardInicio.java
@@ -1,8 +1,7 @@
 package Interface;
 
-import com.studiohartman.jamepad.ControllerManager;
-import com.studiohartman.jamepad.ControllerState;
-
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
@@ -12,34 +11,31 @@ import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.*;
 import javax.swing.Timer;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
 
 public class BoardInicio extends JPanel implements ActionListener{
-	private Image img;
-	private Image button;
-	private Timer time;
-	private JLabel gameStart;
-	private JButton startB;
-    private ControllerManager controllers = new ControllerManager();
+	Image img;
+	Image button;
+	Timer time;
+	JLabel gameStart;
+	JButton startB;
 
-
-
+	
 	public BoardInicio() {
 		ImageIcon g = new ImageIcon("start.jpg"); 
 		img = g.getImage();
-
+		
 		//ImageIcon buttonStart = new ImageIcon("startButton.png"); 
 		//button = buttonStart.getImage();
-
+		
 		startB = new JButton("Start Game");
 		startB.addActionListener(this);
-		this.add(startB);
-
+		
 		time = new Timer(5, this);
 		time.start();
-
-        this.controllers.initSDLGamepad();
 	}
-
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource()==startB) {
@@ -49,34 +45,19 @@ public class BoardInicio extends JPanel implements ActionListener{
 				// TODO Auto-generated catch block
 				e1.printStackTrace();
 			}
-		}
+        }
 		//repaint();
 	}
-
+	
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
 		Graphics2D g2d = (Graphics2D) g;
-
+		
 		g2d.drawImage(img, 0, 0, null);
 		this.add(startB);
-
-        this.buttonPressed();
-
-        repaint();
 		
+		//g2d.drawImage(img, 0, 0, null); pinta imagen pero hay que cambiarla
 	}
-
-	public void buttonPressed() {
-        ControllerState estadoActual = controllers.getState(0);
-
-        if (estadoActual.a) {
-            try {
-                Frame.Start();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
 }
